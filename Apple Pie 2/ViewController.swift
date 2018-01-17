bf0e382002b7d8ad132b13000e748d9d7ab7542c
Emma <egarabedian@lincolnschool.org>
2018-01-17 11:06:51 -0500

    test

--- Apple Pie 2/ViewController.swift
+++ Apple Pie 2/ViewController.swift
@@ -16,18 +16,39 @@ var totalWins = 0
 var totalLosses = 0
 
 class ViewController: UIViewController {
-    
-    
-
-  
    
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
         sender.isEnabled = false
         let letterString = sender.title(for: .normal)!
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
         updateUI()
+        updateGameState()
     }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    
     @IBOutlet var letterButtons: [UIButton]!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var correctWordLabel: UILabel!
@@ -40,16 +61,35 @@ class ViewController: UIViewController {
     var currentGame: Game!
     
     func newRound() {
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord , incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
-        updateUI()
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord , incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
     }
     
     func updateUI() {
+        var letters = [String] ()
+        for letter in currentGame.formattedWord.characters {
+            letters.append(String(letter))
+        }
+        correctWordLabel.text = currentGame.formattedWord
         scoreLabel.text = "Wins\(totalWins), Losses\(totalLosses)"
         treeImageView.image = UIImage (named: "Tree\(currentGame.incorrectMovesRemaining)")
     }
 
+
+    
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
